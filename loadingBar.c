6213478e3e7bf6f625adc197fbb4b6de19d05121
J-Boii <106492855+J-Boii@users.noreply.github.com>
2022-12-26 01:16:10 +0000

    start loading bar

---
+++ loadingBar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void){
+    for(int i = 0; i < 20; i++){
+        printf(" ");
+    }
+    system("clear");
+    printf("#\n");
+    usleep(10000000);
+
+    }
