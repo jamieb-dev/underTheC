6a48eb958080ee5486edc6de89fc9e72b9d9fe88
J-Boii <jamieb2684@gmail.com>
2022-12-27 00:11:29 +0000

    add malloc and refresh funtion

--- loadingBar.c
+++ loadingBar.c
@@ -3,14 +3,35 @@
 #include <unistd.h>
 #include <memory.h>
 
+// prototypes
+void refresh(void);
+
+char *bar;
 
 int main(void){
 
+bar = malloc(25);
+strcpy(bar, "####################");
+int barLen = ((sizeof(bar)) / (sizeof(char)));
+printf("%s\n", bar);
+for(int i = 0; i < 21; i++){
+    printf("");
+}
+for(int i = 0; i < barLen; i++){
+    strcpy(&bar[i], "#");
 
-char *arr = malloc(30 * (sizeof(char)));
+    refresh();
 
-arr = "Hello, world!";
-printf("%s", arr);
-free(arr);
 
+
+
+}
+printf("%s\n", bar);
+free(bar);
 return 0;}
+
+void refresh(void){
+    system("clear");
+    printf("%s\n", bar);
+    usleep(200000);
+    }
